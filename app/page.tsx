b33fd7c06d4b7e2ff5be3a2cf1834c4dc7154453
Arnaud VALANCHAUSKAS <arnaudvalanchauskas@MacBook-Pro-de-Arnaud.local>
2026-03-25 08:10:43 +0100

    Correction app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,5 @@
-'use client'
+import { redirect } from 'next/navigation'
 
-import { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabaseClient'
-import { useRouter } from 'next/navigation'
-
-export default function HomePage() {
-  const router = useRouter()
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const checkUser = async () => {
-      const { data, error } = await supabase.auth.getUser()
-
-      if (error || !data.user) {
-        router.replace('/login')
-        return
-      }
-
-      setLoading(false)
-    }
-
-    checkUser()
-  }, [router])
-
-  if (loading) {
-    return <div style={{ padding: 40 }}>Chargement...</div>
-  }
-
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Dashboard sécurisé</h1>
-      <p>Connexion réussie.</p>
-    </div>
-  )
+export default function Home() {
+  redirect('/login')
 }
